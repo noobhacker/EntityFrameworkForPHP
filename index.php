340fc6ddb298027e88f9ea0784c0a3cb0f1fae98
leesong <leesong1995@gmail.com>
2017-05-20 00:41:16 +0800

    updated on comentary

--- index.php
+++ index.php
@@ -3,13 +3,13 @@ include 'models/context.php';
 $db = new DbContext();
 
 // these are for demonstration purpose
-$results = $db->cats
+$cats = $db->cats
     ->select($db->cats->id)
     ->join($db->cat_names)
     ->where($db->cats->id, 1)
     ->toList();
 
-$results = $db->conts
+$contents = $db->conts
     ->select($db->cont_names->name)
     ->join($db->cont_names, $db->parags)
     ->where($db->conts->id, 1)
