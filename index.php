9be777f819b7a640ba2b06a0bddee7c33fee59a1
leesong <leesong1995@gmail.com>
2017-05-22 18:31:30 +0800

    added testing code for phpstorm

--- index.php
+++ index.php
@@ -5,8 +5,15 @@ $db = new DbContext();
 // these are for demonstration purpose
 $cats = $db->cats
     ->select()
+    ->select()
+    ->select()
+    ->select()
+    ->select()
+    ->select()
+    ->select()
+    ->select()
     ->select();
        
 
 $db->close_conn();
-?>
+
